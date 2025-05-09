fc7e9af9855ebd88255b1fee304c98ae31128422
DanNguyen <bidat2007@gmail.com>
2025-05-09 09:57:01 -0700

    Adding a folder that holds the pictures of the chess pieces

--- src/main/ChessBoard.java
+++ src/main/ChessBoard.java
@@ -1,6 +1,22 @@
 package main;
 
+import java.awt.*;
+
 public class ChessBoard {
 
+    private final int squaresize = 100;
 
+    public void drawBoard(Graphics g)
+    {
+        for(int row=0; row<8; row++)
+        {
+            for(int col=0; col<8; col++)
+            {
+                if( (row+col)%2 == 0)  g.setColor(new Color(255, 255, 204)); //Light squares
+                else g.setColor(new Color(102, 51, 0)); //Dark squares;
+
+                g.fillRect(col * squaresize, row * squaresize, squaresize, squaresize);
+            }
+        }
+    }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -6,8 +6,9 @@ import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable{
     //instance variables
-    private int width = 1200;
-    private int height = 800;
+    private final int width = 1000;
+    private final int height = 800;
+    private final ChessBoard chess_board;
 
     private final int FPS = 60;
     Thread gameThread; //Thread is a class to create a game loop
@@ -17,6 +18,7 @@ public class GamePanel extends JPanel implements Runnable{
     {
         setPreferredSize(new Dimension(width, height));
         setBackground(Color.black);
+        chess_board = new ChessBoard();
     }
 
     //The method starts the game
@@ -29,14 +31,29 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() //Responsible for the game loop
     {
+        double drawInterval = 1000000000.0/FPS; //It takes 16,666,677 nanoseconds = 0.01667 seconds to draw 1 frame
+        long deltaTime = 0;
+        long startTime = System.nanoTime();
+        long currentTime;
+
         while(gameThread!=null)
         {
-            System.out.println("The game is running");
+            currentTime = System.nanoTime();
+            deltaTime += currentTime - startTime;
+            startTime = currentTime;
+            if (deltaTime >= drawInterval) //if the amount of time has passed is >= the draw interval, the screen should be updated
+            {
+                update();
+                repaint();
+                deltaTime -= drawInterval;
+            }
         }
     }
 
     public void update() //shows what is on the screen
     {
+
+
 
     }
 
@@ -44,5 +61,6 @@ public class GamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics graphics)
     {
         super.paintComponent(graphics);
+        chess_board.drawBoard(graphics);
     }
 }
